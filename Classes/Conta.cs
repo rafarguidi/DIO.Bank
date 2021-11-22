9d265579d2c468c51ef061696da465e7902ac353
rafael.guidi <rafael.guidi@cmtecnologia.com.br>
2021-11-22 11:11:27 -0300

    Programa de estudo da DIO, desafio Banco

---
+++ Classes/Conta.cs
@@ -0,0 +1,83 @@
+﻿using DIO.Bank.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIO.Bank.Classes
+{
+    public class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+        public readonly int NumeroConta;
+        private string Titular { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private static int Id = 1;
+
+        public Conta(TipoConta tipo, string titular, double saldo, double credito)
+        {
+            this.TipoConta = tipo;
+            this.NumeroConta = GenerateId();
+            this.Titular = titular;
+            this.Saldo = saldo;
+            this.Credito = credito;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor > Saldo + Credito)
+            {
+                Console.WriteLine("Saldo insuficiente");
+                return false;
+            } 
+            else if (valor > Saldo)
+            {
+                Saldo -= valor;
+                Credito -= Saldo;
+                Console.WriteLine($"Saldo Atual: {Saldo} | Credito: {Credito}");
+                return true;
+            } 
+            else
+            {
+                Saldo -= valor;
+                Console.WriteLine($"Saldo Atual: {Saldo} | Credito: {Credito}");
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            // Saldo negativo e depósito menor
+            if (Saldo < 0 && Saldo < valor*(-1) )
+                Credito += valor;
+            //Saldo negatigo e deposito maior que dívida
+            else if (Saldo < 0 && Saldo > valor * (-1))
+                Credito += Saldo * (-1);
+            
+            Saldo += valor;
+            Console.WriteLine($"Saldo Atual: {Saldo} | Credito: {Credito}");
+        }
+
+        public void Transferir(double valor, Conta contaDestino)
+        {
+            if (Sacar(valor))
+                contaDestino.Depositar(valor);
+        }
+
+        public override string ToString()
+        {
+            var retorno = $"Tipo da Conta:  {TipoConta}" + "\n" +
+                          $"Conta:          {NumeroConta}" + "\n" +
+                          $"Titular:        {Titular}" + "\n" +
+                          $"Saldo:          {Saldo}" + "\n" +
+                          $"Crédito:        {Credito}" + "\n";
+
+            return retorno;
+        }
+
+        private static int GenerateId()
+        {
+            return Id++;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,161 @@
+﻿using DIO.Bank.Classes;
+using DIO.Bank.Enum;
+using System;
+using System.Collections.Generic;
+
+namespace DIO.Bank
+{
+    class Program
+    {
+        public static List<Conta> contas = new List<Conta>();
+        static void Main(string[] args)
+        {
+            string opcao;
+
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("Opções: ");
+                Console.WriteLine("1 - Listar Contas");
+                Console.WriteLine("2 - Adicionar Conta");
+                Console.WriteLine("3 - Transferir");
+                Console.WriteLine("4 - Sacar");
+                Console.WriteLine("5 - Depositar");
+                Console.WriteLine("6 - Limpar Tela");
+                Console.WriteLine("x - Sair");
+                Console.WriteLine();
+                
+                opcao = Console.ReadLine();
+                
+                switch (opcao.ToUpper())
+                {
+                    case "1": // Listar Contas
+                        Console.Clear();
+                        if (contas.Count == 0)
+                            Console.WriteLine("Não há contas cadastradas!");
+
+                        foreach (var c in contas)
+                        {
+                            Console.WriteLine(c.ToString());
+                        }
+                        Console.WriteLine();
+                        break;
+
+                    case "2": // Adicionar Conta
+                        Console.WriteLine("Informe o Tipo da Conta (1 - Pessoa Física ou 2 - Pessoa Jurídica): ");
+                        if (!int.TryParse(Console.ReadLine(), out int tipoConta))
+                        {
+                            Console.WriteLine("O Tipo informado deve ser um número!");
+                            break;
+                        }
+                        if (tipoConta != 1 && tipoConta != 2)
+                        {
+                            Console.WriteLine("Tipo de Conta informado inválido!");
+                            break;
+                        }
+                        Console.WriteLine("Informe o nome do Titular: ");
+                        var nome = Console.ReadLine();
+                        Console.WriteLine("Informe o Saldo: ");
+                        if (!double.TryParse(Console.ReadLine(), out double saldo))
+                            Console.WriteLine("Não foi possível atribuir o valor informado para o Saldo!");
+                        Console.WriteLine("Informe o Crédito: ");
+                        if (!double.TryParse(Console.ReadLine(), out double credito))
+                            Console.WriteLine("Não foi possível atribuir o valor informado para o Crédito!");
+
+                        Conta conta = new Conta((TipoConta)tipoConta, nome, saldo, credito);
+                        contas.Add(conta);
+                        Console.WriteLine();
+                        Console.WriteLine("Conta adicionada!");
+                        Console.WriteLine();
+                        break;
+
+                    case "3": // Transferir
+                        Console.WriteLine("Informe o ID da conta de origem:");
+                        var contaOrigem = BuscarConta();
+                        if (contaOrigem == null)
+                            break;
+
+                        Console.WriteLine("Informe o ID da conta de destino:");
+                        var contaDestino = BuscarConta();
+                        if (contaDestino == null)
+                            break;
+
+                        double? valor = RecebeValor();
+                        if (valor == null)
+                            break;
+
+                        contaOrigem.Transferir((double) valor, contaDestino);
+                        Console.WriteLine("Fim");
+
+                        break;
+
+                    case "4": // Sacar
+                        Console.WriteLine("Informe o ID da conta:");
+                        var contaSaque = BuscarConta();
+                        if (contaSaque == null)
+                            break;
+
+                        double? valorSaque = RecebeValor();
+                        if (valorSaque == null)
+                            break;
+
+                        contaSaque.Sacar((double) valorSaque);
+                        break;
+
+                    case "5": // Depositar
+                        Console.WriteLine("Informe o ID da conta:");
+                        var contaDeposito = BuscarConta();
+                        if (contaDeposito == null)
+                            break;
+
+                        double? valorDeposito = RecebeValor();
+                        if (valorDeposito == null)
+                            break;
+
+                        contaDeposito.Depositar((double)valorDeposito);
+
+                        break;
+
+                    case "6": // Limpar Tela
+                        Console.Clear();
+                        break;
+
+                    case "X":
+                        Console.WriteLine("Até logo!");
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+
+                }
+            } while (opcao.ToUpper() != "X");
+        }
+
+        static Conta BuscarConta()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int idConta))
+            {
+                Console.WriteLine("Número incorreto! Não foi possível completar a operação.");
+                Console.WriteLine();
+            }
+            var conta = contas.Find(c => c.NumeroConta == idConta);
+            if (conta == null)
+            {
+                Console.WriteLine("Não foi possível encontrar a conta informada!");
+                Console.WriteLine();
+            }
+            return conta;
+        }
+
+        static double RecebeValor()
+        {
+            Console.WriteLine("Informe o valor:");
+            if (!double.TryParse(Console.ReadLine(), out double valor))
+            {
+                Console.WriteLine("Valor incorreto! Não foi possível completar a operação.");
+                Console.WriteLine();
+            }
+            return valor;
+        }
+    }
+}
